542950afe4a3c163f280c4161d56b2f85c59e44d
Liam Ronan <liamronan16@gmail.com>
2023-12-03 00:25:14 +0000

    arrays, objects

---
+++ dist/index.js
@@ -0,0 +1,34 @@
+"use strict";
+console.log("hello bros!!!!");
+////////////
+/* Arrays */
+////////////
+let names = ["Mario", "Peach", "luigi", "bowser"];
+let ages = [65, 12, 23, 76, 54];
+names.push("hello");
+ages.push(21);
+let fruits = ["apples", "pears", "mangos", "bananas"];
+fruits.push("peaches");
+const f = fruits[3];
+let things = [1, true, "hello"];
+const t = things[0];
+////////////
+/* Object Literals */
+////////////
+let user = {
+    firstName: "mario",
+    age: 23,
+    id: 4,
+};
+user.firstName = "liam";
+user.id = 5;
+////////////
+/* Type interface - object literals */
+////////////
+let person = {
+    name: "luigi",
+    score: 34
+};
+person.name = "ellie";
+person.score = 43;
+const score = person.score;
